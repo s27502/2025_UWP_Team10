b87784899f9e1f4a5f9c2462c0d7820d9ce9c225
s26697 <s26697@pjwstk.edu.pl>
2025-04-16 00:16:27 +0200

    dodano obrót w kierunku ruchu dla przeciwnika

--- Tower Defense/Assets/Scripts/Enemies/Enemy.cs
+++ Tower Defense/Assets/Scripts/Enemies/Enemy.cs
@@ -11,7 +11,7 @@ namespace Enemies
         protected Transform[] path;
         private int pathIndex = 0;
         private const float PathThreshold = 0.1f;
-
+        private Vector3 direction;
         public void Initialize(EnemyData enemyData)
         {
             data = enemyData;
@@ -37,6 +37,12 @@ namespace Enemies
             Transform targetPoint = path[pathIndex];
             float _speed = data.Speed * Time.deltaTime;
 
+            direction = (targetPoint.position - transform.position).normalized;
+            if (direction != Vector3.zero)
+            {
+                transform.forward = direction;
+            }
+            
             transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, _speed);
 
             if (Vector3.Distance(transform.position, targetPoint.position) < PathThreshold)
