4c3569923f51c01ef1f7cbee92f61c3f2287133a
s27466 <s27466@pjwstk.edu.pl>
2025-06-09 23:13:06 +0200

    tutorial dziala

--- Tower Defense/Assets/Scripts/Enemies/Enemy.cs
+++ Tower Defense/Assets/Scripts/Enemies/Enemy.cs
@@ -71,15 +71,15 @@ namespace Enemies
 
             if (currentHP <= 0)
             {
-                Die();
+                Kill();
             }
             ServiceLocator.Instance.GetService<AudioManager>().PlayClip("SFX", "Enemy_Hit");
         }
 
         public void Die()
         {
-            // TODO play VFX, sound and increase money
-            ServiceLocator.Instance.GetService<ResourceManager>().AddMoney(data.Reward);
+            // TODO play VFX, sound
+
             ServiceLocator.Instance.GetService<WaveManager>()?.RemoveEnemy(this); //#TODO probably subscribe to OnEnemyDeath method in gamemanager and then pass this to wave manager
             //Destroy(gameObject);
             _pool?.ReleaseObject(this);
@@ -87,7 +87,7 @@ namespace Enemies
 
         public void Kill()
         {
-            ServiceLocator.Instance.GetService<ResourceManager>()?.AddMoney(data.Reward);
+            ServiceLocator.Instance.GetService<ResourceManager>().AddMoney(data.Reward);
             Die();
         }
 
--- Tower Defense/Assets/Scripts/Tutorial/TutorialUIManager.cs
+++ Tower Defense/Assets/Scripts/Tutorial/TutorialUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Tutorial
@@ -5,30 +6,111 @@ namespace Tutorial
     public class TutorialUIManager : MonoBehaviour
     {
         [SerializeField] private GameObject attackMessage;
-        private bool _isAttackMessageActivated;
-    
-        private void Start()
+        [SerializeField] private GameObject welcomeMessage;
+        [SerializeField] private GameObject upgradeMessage;
+        [SerializeField] private GameObject placingField;
+
+        private int lastHP = -1;
+        private bool welcomeMessageClosed = false;
+        private bool upgradeMessageClosed = false;
+        private bool moneyChangeHandled = false;
+        private bool wasAttacked = false;
+        private bool isUpgradeCoroutineRunning = false;
+
+        private void Awake()
         {
-            _isAttackMessageActivated = false;
+            welcomeMessage.SetActive(true);
+            upgradeMessage.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            var resourceManager = ServiceLocator.Instance.GetService<ResourceManager>();
+            if (resourceManager != null)
+            {
+                resourceManager.OnHpChanged += HandleHpChanged;
+                resourceManager.OnMoneyChanged += HandleMoneyChanged;
+                lastHP = resourceManager.GetHP();
+            }
+        }
+
+        private void OnDisable()
+        {
+            var resourceManager = ServiceLocator.Instance.GetService<ResourceManager>();
+            if (resourceManager != null)
+            {
+                resourceManager.OnHpChanged -= HandleHpChanged;
+                resourceManager.OnMoneyChanged -= HandleMoneyChanged;
+            }
         }
 
         private void Update()
         {
-            
+            if (!welcomeMessageClosed && Input.GetKeyDown(KeyCode.Space))
+            {
+                welcomeMessage.SetActive(false);
+                welcomeMessageClosed = true;
+                Debug.Log("Welcome message closed.");
+            }
+
+            if (upgradeMessage.activeSelf && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                upgradeMessage.SetActive(false);
+                upgradeMessageClosed = true;
+                Debug.Log("Upgrade message closed by mouse click.");
+            }
+        }
+
+        private void HandleHpChanged(int newHP)
+        {
+            if (wasAttacked)
+                return;
+
+            if (newHP < lastHP)
+            {
+                ShowAttackMessage();
+            }
+            lastHP = newHP;
+            wasAttacked = true;
+        }
+
+        private void HandleMoneyChanged(int newMoney)
+        {
+            if (moneyChangeHandled) return;
+
+            HideAttackMessage();
+            moneyChangeHandled = true;
         }
 
         public void ShowAttackMessage()
         {
-            if (_isAttackMessageActivated) return;
-            _isAttackMessageActivated = true;
-            Time.timeScale = 0.5f;
-            attackMessage.gameObject.SetActive(true);
+            attackMessage.SetActive(true);
+            placingField.SetActive(true);
+            Debug.Log("Attack message shown.");
         }
 
         public void HideAttackMessage()
         {
-            Time.timeScale = 1f;
-            attackMessage.gameObject.SetActive(false);
+            attackMessage.SetActive(false);
+            Debug.Log("Attack message hidden.");
+
+            if (!isUpgradeCoroutineRunning)
+            {
+                Debug.Log("Starting coroutine to show upgrade message...");
+                StartCoroutine(OpenUpgradeMessageWithDelay(5f));
+            }
+        }
+
+        private IEnumerator OpenUpgradeMessageWithDelay(float delay)
+        {
+            isUpgradeCoroutineRunning = true;
+            yield return new WaitForSeconds(delay);
+
+            upgradeMessageClosed = false;
+            upgradeMessage.SetActive(true);
+            Debug.Log("Upgrade message shown.");
+
+            isUpgradeCoroutineRunning = false;
         }
     }
 }
