144c9d855185f93abcb29e1a3027efce737fa1d1
s27466 <s27466@pjwstk.edu.pl>
2025-06-09 21:20:21 +0200

    wydzielenie części wpólnej wież

--- Tower Defense/Assets/Scripts/Towers/Tower.cs
+++ Tower Defense/Assets/Scripts/Towers/Tower.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Enemies;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Towers
@@ -6,12 +8,76 @@ namespace Towers
     public class Tower : MonoBehaviour, IPointerClickHandler, ITower
     {
         [SerializeField] protected TowerData data;
-        private PlacingField _placingField;                                             
+        protected PlacingField _placingField;                                             
         private GameObject _upgradePanel;
         private bool _upgradePanelActive;
-        private IPointerClickHandler _pointerClickHandlerImplementation;
-        
-        
+
+        protected List<Enemy> enemiesInRange = new();
+        private float attackCooldown;
+
+        protected virtual string GetEnemyTag() => "Enemy";
+        protected virtual string GetAttackSound() => "Default_Shot";
+
+        private void Update()
+        {
+            if (!gameObject.activeSelf) return;
+
+            attackCooldown -= Time.deltaTime;
+            if (attackCooldown <= 0f && enemiesInRange.Count > 0)
+            {
+                Defend();
+                attackCooldown = data.AttackSpeed;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(GetEnemyTag()))
+            {
+                Enemy enemy = other.GetComponent<Enemy>();
+                if (enemy != null && !enemiesInRange.Contains(enemy))
+                    enemiesInRange.Add(enemy);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(GetEnemyTag()))
+            {
+                Enemy enemy = other.GetComponent<Enemy>();
+                if (enemy != null && enemiesInRange.Contains(enemy))
+                    enemiesInRange.Remove(enemy);
+            }
+        }
+
+        public virtual void Defend()
+        {
+            if (enemiesInRange.Count == 0) return;
+
+            Enemy target = enemiesInRange[0];
+
+            if (target == null)
+            {
+                enemiesInRange.RemoveAt(0);
+                return;
+            }
+
+            Vector3 direction = target.transform.position - transform.position;
+            direction.y = 0f;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = lookRotation;
+            }
+
+            Debug.Log($"Attacking enemy: {target.name} for {data.Damage} dmg");
+            target.TakeDamage(data.Damage);
+
+            string sound = GetAttackSound();
+            ServiceLocator.Instance.GetService<AudioManager>().PlayClip("SFX", sound);
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
             if (!_upgradePanelActive)
@@ -26,29 +92,21 @@ namespace Towers
                 _upgradePanelActive = true;
             }
         }
-        
-        
+
         public void setUpgradePanelActive(bool active)
         {
             _upgradePanelActive = active;
         }
 
-        public virtual void Defend(){}
-       
-        
         public string GetTowerName() => data.TowerName;
         public int GetCost() => data.Cost;
         public float GetAttackSpeed() => data.AttackSpeed;
         public float GetDamage() => data.Damage;
+        public TowerData GetData() => data;
 
-        public TowerData GetData()
-        {
-            return data;
-        }
         public void SetPlacingField(PlacingField placingField)
         {
             _placingField = placingField;
         }
-        
     }
 }
