02f49672e6ca302c91a553f72baa94c9377ac2f8
s27466 <161813484+s27466@users.noreply.github.com>
2025-06-10 22:42:52 +0200

    wave manager zmieniony na destroy

---
+++ Tower Defense/Assets/Scripts/Managers/SingletonDoDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SingletonDoDestroy<T> : MonoBehaviour where T : Component
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this as T;
+    }
+}
